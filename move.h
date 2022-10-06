73cbee5a7ea3017d9a61acf020e074cfb6487799
Jane Orr <djaneorr@gmail.com>
2022-10-06 17:01:22 -0600

    edited move.h

--- move.h
+++ move.h
@@ -32,25 +32,54 @@ private:
    string error;
    
 public:
+   // Getters
    Move() {}
-   string getText() { return "Michael"; }
-   Position getSrc() { return * new Position; }
-   Position getDes() { return * new Position; }
-   char getPromotion() { return 'M'; }
-   char getCapture() { return 'M'; }
-   bool getEnPassant() { return false; }
-   bool getCastleK() { return false; }
-   bool getCastleQ() { return false; }
-   bool getWhiteMove() { return false; }
-   void setEnPassant() {}
-   void setCastle(bool isKing) {}
-   void setCastleQ() {}
-   void setWhiteMove() {}
+   string getText()           const { return "Michael"; }
+   const Position & getSrc()  const { return source; }
+   const Position & getDes()  const { return dest; }
+   char getPromotion()        const { return piece; }
+   char getCapture()          const { return capture; }
+   bool getEnPassant()        const { return enpassant; }
+   bool getCastleK()          const { return castleK; }
+   bool getCastleQ()          const { return castleQ; }
+   bool getWhiteMove()        const { return isWhite; }
+
+   // Setters
+   void getCapture(char letter) { capture = tolower(letter); }
+   void setWhiteMove(bool f) { isWhite = f; }
+   void setSrc(const Position & src) { source = src; }
+   void setDes(const Position& des) { dest = des; }
+   void setEnPassant() { enpassant = true; }
+   void setPromote(char letter) { piece = letter; }
+   void complete(const Board& board) {}
+   void setCastle(bool isKing)
+   {
+      if (isKing)
+         castleK = true;
+      else
+         castleQ = true;
+   };
+   
    void assign(Move move) {}
    void assign(string move) {}
    // operator <<()
    // operator >>()
    bool operator< (const Move & rhs) const {
       return true;
+   }
+   // friend ostream & operator << (ostream& out, Move & rhs);
+   // friend istream & operator >> (istream& in, Move& rhs);
+
+   //const Move & operator = (const Move & rhs);
+   const Move& operator = (const string& s)
+   {
+      //read(s);
+      return *this;
+   }
+   const Move& operator = (const char* s)
+   {
+      const string str(s);
+      //read(str);
+      return *this;
    }
 };
