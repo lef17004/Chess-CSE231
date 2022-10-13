0b1e55c9e3e5a1495a6128c86eafab5ab3b1b033
ColletteStapley <sta18019@byui.edu>
2022-10-12 18:29:57 -0600

    Added Deltas for Knight

--- knight.cpp
+++ knight.cpp
@@ -9,6 +9,14 @@
 // TODO: Move to header file.
 set<Move> * Knight::getPossibleMoves(const Board & board)
 {
+   array<Delta, 8> deltas = 
+   {
+  
+                          Delta(- 1,  2), Delta( 1,  2),
+           Delta(-2,  1),                             Delta( 2,  1),
+           Delta(-2, -1),                             Delta( 2, -1),
+                           Delta(- 1, -2), Delta(1, -2)
+   };
    return new set<Move>();
 }
 
